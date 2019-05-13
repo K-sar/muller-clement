d20943fcd22efa3799fe84826377e15823633f7d
K-sar <muller.clement@laposte.net>
2019-05-13 15:13:56 +0200

    message flash et validator

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -5,11 +5,14 @@
 @endsection
 
 @section("content")
+    @if ($errors->any())
+        <ul>{!! implode('', $errors->all('<li style="color:red">:message</li>')) !!}</ul>
+    @endif
     <form method="post" action="{{route('folder_store')}}">
         @csrf
-        <input type="text" name="name" />
-        <input type="text" name="slug" />
-        <input type="text" name="access" />
+        <input type="text" name="name" value="{{old("name")}}" />
+        <input type="text" name="slug" value="{{old("slug")}}" />
+        <input type="text" name="access" value="{{old("access")}}" />
         <input type="submit" />
     </form>
 @endsection
