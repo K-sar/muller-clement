060f8e4275838525334fc74b80caf5bfabc9655c
K-sar <muller.clement@laposte.net>
2019-11-08 01:31:45 +0100

    Xp déployé

--- resources/views/bases/CV/xp/create.blade.php
+++ resources/views/bases/CV/xp/create.blade.php
@@ -1,69 +1,51 @@
 @extends ("layouts.layout")
 
 @section('title')
-    <title>Clément Muller - Galerie - {{$folder->name}} - Ajouter une photo</title>
-    <meta name="description" content="Formulaire d'ajout d'une nouvelle photo"/>
+    <title>Clément Muller - CV - Ajouter une {{old("type")?:$type}}</title>
+    <meta name="description" content="Formulaire d'ajout d'une nouvelle {{old("type")?:$type}}"/>
 @endsection
 
 @section("nav")
-    <div><a href="{{route('folder.show', $folder->slug)}}"><h3><i class="fas fa-arrow-left"></i></h3></a></div>
+    <div><a href="{{route('CV.backoffice')}}"><h3><i class="fas fa-arrow-left"></i></h3></a></div>
 @endsection
 
 @section("content")
     @if ($errors->any())
         <ul>{!! implode('', $errors->all('<li style="color:red">:message</li>')) !!}</ul>
     @endif
-    <form method="post" action="{{route('folder.picture.store', $folder->slug)}}" enctype="multipart/form-data">
+    <form method="post" action="{{route('xp.store')}}" enctype="multipart/form-data">
         @csrf
         <div>
             <legend>
-                <h2>Ajouter une nouvelle photo</h2>
+                <h2>Ajouter une nouvelle {{old("type")?:$type}}</h2>
             </legend>
         </div>
         <div>
-            <label>Fichier :</label>
-            <input type="file" name="file"/>
-        </div>
-        <div>
-            <label>Dossier :</label>
-            <select name="folder_id">
-                @foreach ($allFolders as $allFolder)
-                    <option @if (old("folder_id")==$allFolder->id||(!old("folder_id")&&$allFolder->id==$folder->id)) selected @endif value="{{$allFolder->id}}">{{$allFolder->name}}</option>
-                @endforeach
+            <label>Type :</label>
+            <select name="type">
+                <option @if (old("type") == "expérience"||(!old("type")&&$type=='expérience')) selected @endif value="expérience">Expérience</option>
+                <option @if (old("type") == "formation"||(!old("type")&&$type=='formation')) selected @endif value="formation">Formation</option>
             </select>
         </div>
         <div>
-            <label>Nom :</label>
-            <input type="text" name="name" value="{{old("name")}}" />
+            <label>Titre :</label>
+            <input type="text" name="title" value="{{old("title")}}" />
         </div>
         <div>
-            <label>Accès :</label>
-            <input type="text" name="access" value="{{old("access")}}" />
+            <label>Description :</label>
+            <input type="text" name="content" value="{{old("content")}}" />
         </div>
         <div>
-            <label>Infos :</label>
-            <input type="text" name="info" value="{{old("info")}}" />
+            <label>De :</label>
+            <input type="text" name="from" value="{{old("from")}}" />
         </div>
         <div>
-            <label>Texte alternatif :</label>
-            <input type="text" name="alternative" value="{{old("alternative")}}" />
+            <label>A :</label>
+            <input type="text" name="to" value="{{old("to")}}" />
         </div>
         <div>
-            <label>Ordre :</label>
-            <input type="text" name="ordre" value="{{old("ordre")}}" />
-        </div>
-        <div>
-            <label>Slider :</label>
-            <input type="text" name="slider" value="{{old("slider")}}" />
-        </div>
-        <div>
-            <label>Tags :</label>
-            <p>
-                @foreach($allTags as $tag)
-                    <button class="clicTag" data-value="{{$tag->name}}">{{$tag->name}}</button>
-                @endforeach
-            </p>
-            <input id="inputTag" type="text" name="tags" value="{{old("tags")}}" />
+            <label>Lien :</label>
+            <input type="text" name="link" value="{{old("link")}}" />
         </div>
         <div>
             <input type="submit" />
